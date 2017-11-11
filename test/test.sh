d7d745872ce9219f85fabfbfd67601c4d30dbfcc
Anton Kvashenkin <anton.jugatsu@gmail.com>
2017-11-11 19:43:41 +0300

    Add test.sh script

---
+++ test/test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# exit on any error
+set -e
+
+# check for syntax error
+molecule syntax
+
+# lint with ansible-lint
+molecule lint
+
+# converge
+molecule converge
+
+# sleep for 10 seconds
+sleep 10
+
+# verify with inspec
+inspec --version
+inspec exec --no-color test/smoke/default -t docker://xenial
+inspec exec --no-color test/smoke/default -t docker://trusty
+inspec exec --no-color test/smoke/default -t docker://precise
+
+# clean up
+molecule destroy
